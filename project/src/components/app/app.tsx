207ca82ad0cc2b2ed00e4539e896a19d5fae8441
Vladislav Zhuravlev <vladislavzhuravlev@Vladislavs-MacBook-Pro.local>
2022-03-28 23:34:22 +0300

    Add components

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../main-page/main';
+
+type AppScreenProps = {
+  amountOffers: number;
+}
+
+function App({amountOffers}: AppScreenProps): JSX.Element {
+  return (
+    <MainPage amountOffers={amountOffers} />
+  );
 }
 
 export default App;
